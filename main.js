fa65397c93a01ba6279280ad08b7082686840ea4
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2024-04-22 00:02:17 +0500

    project almost compelete

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 #! /usr/bin/env node
-export {};
+import inquirer from "inquirer";
+//declare a constant "answer" assign it to the result of inquirer.prompt function
+let condition = true;
+while (condition) {
+    const answers = await inquirer.prompt([
+        {
+            name: "sentence",
+            type: "input",
+            message: "Enter your sentence:"
+        },
+        {
+            name: "more",
+            message: "Do you want to continue?",
+            type: "list",
+            choices: ["Yes", "No"],
+            default: "false",
+        }
+    ]);
+    console.log();
+    const words = answers.sentence.trim().split(" ");
+    console.log(words);
+    //print the word count of the sentence
+    console.log(`Your sentence contains ${words.length} words and ${answers.sentence.length} characters.`);
+    if (answers.more === "No") {
+        condition = false;
+    }
+}
